fd02a733274e36b193ec46931cf779ebfe9b0e48
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-11 21:02:03 +0700

    add other packages in nix on droid

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -4,11 +4,15 @@
   # Simply install just the packages
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
-    vim # or some other editor, e.g. nano or neovim
-    nano
-    man
+    # Make sure its CLI only
+    bat
+    fastfetch
     git
+    man
+    nano
     tldr
+    tmate
+    vim
     yt-dlp
   ];
 
