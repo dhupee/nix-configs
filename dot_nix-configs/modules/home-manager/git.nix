96b7f903ad0325d133e7dfc5dab68507cad59a69
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-19 01:51:47 +0700

    try to repair git, not sure why

--- dot_nix-configs/modules/home-manager/git.nix
+++ dot_nix-configs/modules/home-manager/git.nix
@@ -3,18 +3,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "Daffa Haj Tsaqif";
-    userEmail = "narutohaj00@gmail.com";
     lfs = {
       enable = true;
-    # extraConfig = {
-    #     ''
-    #     [credential "https://github.com"]
-    #             helper = !$(which gh) auth git-credential
-    #     [credential "https://gist.github.com"]
-    #             helper = !$(which gh) auth git-credential
-    #     ''
-    #   };
+    extraConfig = {
+        user.name = "Daffa Haj Tsaqif";
+        user.email = "narutohaj00@gmail.com";
+      };
     };
   };
 }
